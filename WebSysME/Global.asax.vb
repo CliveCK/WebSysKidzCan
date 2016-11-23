6968f86088a11d8fd5ae1e31fe5c88768fb36370
CliveCK <clivenyasondo@gmail.com>
2016-11-23 18:40:44 +0200

    Init Commit of files

---
+++ WebSysME/Global.asax.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Web.Routing
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Sub Application_Start(sender As Object, e As EventArgs)
+        RouteConfig.RegisterRoutes(RouteTable.Routes)
+        'BundleConfig.RegisterBundles(BundleTable.Bundles)
+    End Sub
+End Class
